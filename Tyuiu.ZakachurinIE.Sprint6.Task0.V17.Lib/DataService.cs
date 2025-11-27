effca0ee97618b84fbab9832174f5ec7ea7821e4
Иван Закачурин <vanekcarek43@gmail.com>
2025-11-27 23:29:51 +0500

    Task0.3

---
+++ Tyuiu.ZakachurinIE.Sprint6.Task0.V17.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.ZakachurinIE.Sprint6.Task0.V17.Lib
+{
+    public class DataService : ISprint6Task0V17
+    {
+        public double Calculate(int x)
+        {
+            double res = Math.Pow(Math.E, x) / x;
+            return Math.Round(res, 3);
+        }
+    }
+}
